d46226d2c922bab4163b4e51eadd7bc8ee6e0809
Nicole Luong <z5206727@unsw.edu.au>
2023-05-29 22:08:39 +1000

    Ready for tutorial

---
+++ week_1/hello_world.c
@@ -0,0 +1,42 @@
+// Basic Hello World Program
+// Nicole, 23T2 Week 1
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello World\n");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Change the code to print this face
+// ~ ~
+// 0 0
+//  o
+//  -
+
+// Change the code to print this cat
+//  /\___/\
+// (=' w '=)
