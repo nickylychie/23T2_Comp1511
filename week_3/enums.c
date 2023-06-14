caad6c0527f4278f565fca68606d70ef541b857b
Nicole <z5206727@unsw.edu.au>
2023-06-14 10:13:51 +1000

    Tutorial demo code

--- week_3/enums.c
+++ week_3/enums.c
@@ -6,19 +6,23 @@
 // what are the different parts of this enum?
 // enum opal_card_type is the name of this new datatype
 enum opal_card_type {
-    ADULT, 
-    CONCESSION, 
-    CHILD
+    ADULT, // 0
+    CONCESSION = 10, 
+    CHILD, // 1
+    STUDENT // 2
 };
 
 int main(void) {
 
     // create a variable to store an enum?
+    enum opal_card_type nicole = STUDENT;
     // "enum opal_card_type" is the datatype
 
     printf("adult is of type      %d\n", ADULT);
     printf("concession is of type %d\n", CONCESSION);
     printf("child is of type      %d\n", CHILD);
+
+    printf("I have opal type: %d\n", nicole);
     
     return 0;
 }
--- week_3/structs.c
+++ week_3/structs.c
@@ -13,11 +13,18 @@ struct person {
 int main(void) {
 
     // initialise struct person
+    struct person tommy;
+    tommy.height = 2.5;
+    tommy.first_name_initial = 't';
+    tommy.fav_number = 1;
     
     // initialise the values inside the struct person you 
     // just created
 
     // print a struct person
+
+    printf("Tommy is %lfcm tall, his name starts with %c, and his fav number is %d", 
+    tommy.height,tommy.first_name_initial, tommy.fav_number);
     
     return 0;
 }
